a4d2b46ef772371e29edd84863839fbaa3e959aa
Khanh Nguyen <vnsquall@gmail.com>
2015-06-02 17:40:59 +0700

    init this repos

---
+++ src/test/java/Utils/SetupTest.java
@@ -0,0 +1,27 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by khanh.nguyen on 6/2/2015.
+ */
+public class SetupTest {
+    public static WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/testcases/TestLoginPage.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import Utils.SetupTest;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+/**
+ * Created by khanh.nguyen on 6/2/2015.
+ */
+public class TestLoginPage extends SetupTest{
+
+    @DataProvider
+    Object[][] getCredentials() {
+        return new Object[][]{
+                {"tomsmith", "SuperSecretPassword!"},
+                {"a12345", "Mr Test"}
+        };
+    }
+
+    @Test(dataProvider =  "getCredentials")
+    public void testLoginPage(String user, String pass){
+        LoginPage loginpage = new LoginPage();
+        Assert.assertEquals(loginpage.loginPageLoaded(driver), Boolean.TRUE);
+        loginpage.loginToLoginPage(driver, user, pass);
+    }
+}
