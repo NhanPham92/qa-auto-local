9d840319badd7d5cf4be2a477917ffea03d69bf8
Khanh Nguyen <vnsquall@gmail.com>
2015-06-09 17:44:07 +0700

    Revert "Revert "Change into *.java file""
    This reverts commit 4478a65164b35758d2ea19b730d72af44004b330.

--- src/test/java/testcases/TestDropDown.java
+++ src/test/java/testcases/TestDropDown.java
@@ -2,7 +2,6 @@ package testcases;
 
 import Utils.SetupTest;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DropDownPage;
